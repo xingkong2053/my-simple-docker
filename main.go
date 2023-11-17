972f0b93fde300135b481a9cb81f7e800fbe9ea2
xingkong2053 <lipenghui2053@gmail.com>
2023-11-17 15:42:21 +0800

    set debug level

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "mydocker/cmd"
+import (
+	"github.com/sirupsen/logrus"
+	"mydocker/cmd"
+)
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
 	cmd.Execute()
 }
